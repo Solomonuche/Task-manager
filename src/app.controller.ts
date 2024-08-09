f075ff37e81c8334bdd9cad0b835429a62f73531
Solomon <solomonuche@gmail.com>
2024-08-09 01:14:48 +0100

    feat: added api status route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SkipAuth } from './decorators/skipAuth.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @SkipAuth()
+  @Get("status")
+  getStatus() {
     return this.appService.getHello();
   }
 }
